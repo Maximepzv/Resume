f337b54b38f1920fca82e78c9befe358a541b736
Maximepzv <maxime.pouezevara@epitech.eu>
2019-01-28 22:57:11 +0100

    header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { Routing, RoutedComponents } from './routings/routing';
 import { AppComponent } from './components/app/app.component';
@@ -18,7 +19,8 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     Routing,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
